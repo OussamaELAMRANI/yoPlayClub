434507fd048dbaa3016e6071e9de1af301bfaeac
Oussama EL AMRANI <oelamrani@sql.com>
2021-07-22 13:12:51 +0100

    env(heroku): setup twig & maker with 1st yo:controller

---
+++ src/Infrastructure/Maker/AbstractMakerCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Infrastructure\Maker;
+
+use Symfony\Component\Console\Command\Command;
+use Twig\Environment;
+
+abstract class AbstractMakerCommand extends Command
+{
+    public function __construct(
+        string|null $name,
+        protected Environment $twig,
+        protected string $projectDir
+    )
+    {
+        parent::__construct($name);
+    }
+
+    protected function createFile(string $template, array $params, string $output): void
+    {
+        $content = $this->twig->render("@maker/{$template}.twig", $params);
+        $filename = "{$this->projectDir}/{$output}";
+        $directory = dirname($filename);
+        if (!file_exists($directory)) {
+            mkdir($directory, 0777, true);
+        }
+        @file_put_contents($filename, $content);
+    }
+}
